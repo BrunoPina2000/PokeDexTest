fdbbfe248dbde0904921b65ce0458adb536dcb1a
Sinuhe Arturo Evia Campos <furrymax0w0@gmail.com>
2023-02-21 20:45:55 -0600

    Funciones de debilidades y linea evolutiva

--- App.tsx
+++ App.tsx
@@ -1,39 +1,56 @@
-import { useState} from 'react'
-import { StyleSheet, Text, View ,TextInput} from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import PokemonList from './components/PokemonList';
 
 export default function App() {
-  const [pokeId, setPokeId] = useState('')
-  
+  const [pokeId, setPokeId] = useState('');
 
-  const {pokemonName,pokemonType,pokemonDescription,pokemonMoves} = PokemonList(pokeId)
-  //const upDateSearchBar = (value:string) =>(setPokeId(value))
+  const {
+    pokemonName,
+    pokemonType,
+    pokemonDescription,
+    pokemonMoves,
+    pokemonEvolution,
+    pokemonDamageRelations,
+  } = PokemonList(pokeId);
+
   return (
-    <View style = {styles.container}>
+    <View style={styles.container}>
       <TextInput
-      placeholder="Ingresa Dato"
-      onChangeText={setPokeId}
-      value = {pokeId}
+        placeholder="Ingresa Dato"
+        onChangeText={setPokeId}
+        value={pokeId}
       />
-      
-      <Text>{pokemonName}</Text>
-      <Text>{pokemonType}</Text>
-      <Text>{pokemonDescription}</Text>
-      <Text>{pokemonMoves}</Text>
-      
-     
-      
+
+      {pokeId === '' ? (
+        <Text></Text>
+      ) : (
+        <View>
+          <Text>{pokemonName}</Text>
+          <Text></Text>
+          <Text>{pokemonType}</Text>
+          <Text></Text>
+          <Text>{pokemonDescription}</Text>
+          <Text></Text>
+          <Text>{JSON.stringify(pokemonDamageRelations)}</Text>
+          <Text></Text>
+          <Text>{pokemonMoves}</Text>
+          <Text></Text>
+          <Text>{pokemonEvolution}</Text>
+          
+        </View>
+      )}
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:80,
+    marginTop: 80,
     flex: 0,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textAlign: 'center',
   },
 });
--- components/PokemonList.tsx
+++ components/PokemonList.tsx
@@ -1,65 +1,95 @@
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, View , Text, FlatList} from 'react-native';
+import React, { useEffect, useState } from 'react';
 import instanceApi from './InstanceApi';
 import instanceDesct from './InstanceDesct';
+import axios from 'axios';
 
-
-
-
-
-export default function PokemonList(PokeId:string){
-    const [pokemonName, setPokemonName] = useState();
-    const [pokemonType, setPokemonType] = useState([]);
-    const [pokemonDescription, setPokemonDescription] = useState();
-    const [pokemonMoves, setPokemonMoves] = useState();
-    const [evolutionChain, setEvolutionChain] = useState([]);
-
-    
-
-
-    
-    useEffect(() => {
-        const fetchPokemon = async ()=> {
-            try {
-                const {data} = await instanceApi.get(`/${PokeId}`);
-                setPokemonName(data.name);
-                const types = data.types.map(type=> type.type.name);
-                setPokemonType(types.join(','));
-                const moves = data.moves.map(move => move.move.name).sort();
-
-                setPokemonMoves(moves.join(','))
-
-                instanceDesct.get(`/${PokeId}`).then(datos => {
-                    const desc = datos.data.flavor_text_entries.find(entry => entry.language.name === 'es').flavor_text;
-                    setPokemonDescription(desc);
-                })
-
-                
-                
-                console.log(pokemonDescription)
-
-
-               
-            }
-            catch(error) {
-                console.log(error);
-            }
-            
+const dmg = async (tipo: any) => {
+  const result = await Promise.all(tipo.map(async (url:any) => {
+    const { data } = await axios.get(url);
+    return data.damage_relations;
+  }));
+  return result.reduce((acc: any, curr: any) => {
+    Object.entries(curr).forEach(([key, value]) => {
+      if (Array.isArray(value)) {
+        if (!acc[key]) {
+          acc[key] = [];
         }
-        fetchPokemon();
-        
-    },[PokeId])
-    
-  
-    
+        value.forEach((val: any) => {
+          if (!acc[key].includes(val.name)) {
+            acc[key].push(val.name);
+          }
+        });
+      }
+    });
+    return acc;
+  }, {});
+};
 
-    return{
-        pokemonName, 
-        pokemonType,
-        pokemonDescription,
-        pokemonMoves,
-        evolutionChain
-    }
+let pokemones: any = [];
 
+function evolution(pokemon: any) {
+  if (pokemon.length == 1) {
+    pokemones.push(pokemon[0].species.name);
+    evolution(pokemon[0].evolves_to);
+  }
+  if (pokemon.length > 1) {
+    pokemon.forEach((pokemonForEach: any) => {
+      pokemones.push(pokemonForEach.species.name);
+    });
+  }
 }
 
+export default function PokemonList(PokeId: string) {
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonType, setPokemonType] = useState([]);
+  const [pokemonDescription, setPokemonDescription] = useState('');
+  const [pokemonMoves, setPokemonMoves] = useState('');
+  const [pokemonEvolution, setPokemonEvolution] = useState([]);
+  const [pokemonDamageRelations, setPokemonDamageRelations] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const { data } = await instanceApi.get(`/${PokeId}`);
+
+        setPokemonName(data.name);
+
+        const types = data.types.map((type: any) => type.type.name);
+        setPokemonType(types);
+
+        const damageRelations = await dmg(data.types.map((type: any) => type.type.url));
+        setPokemonDamageRelations(damageRelations);
+
+        const moves = data.moves.map((move: any) => move.move.name).sort();
+        setPokemonMoves(moves.join(','));
+
+        instanceDesct.get(`/${PokeId}`).then(async (evol) => {
+          pokemones = [];
+          const evolLine = evol.data.evolution_chain.url;
+          const chainchEvol = await axios.get(evolLine);
+          const chainchE = chainchEvol.data.chain;
+          pokemones.push(chainchE.species.name);
+          evolution(chainchE.evolves_to);
+          setPokemonEvolution(pokemones.join(' => '));
+        });
+
+        instanceDesct.get(`/${PokeId}`).then((datos) => {
+          const desc = datos.data.flavor_text_entries.find((entry: any) => entry.language.name === 'es').flavor_text;
+          setPokemonDescription(desc);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPokemon();
+  }, [PokeId]);
+
+  return {
+    pokemonName,
+    pokemonType,
+    pokemonDescription,
+    pokemonMoves,
+    pokemonEvolution,
+    pokemonDamageRelations,
+  };
+}
